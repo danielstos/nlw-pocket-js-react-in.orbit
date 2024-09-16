ca34f840de93c89a8df665ffab4633742029cd0a
danielstos <daniel.jose.stos@gmail.com>
2024-09-15 21:42:19 -0300

    feat: front page style, add ui and components folders

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,23 @@
+import logo from "./assets/logo-in-orbit.svg";
+import letsStart from "./assets/lets-start-illustration.svg";
+import { Plus } from "lucide-react";
+
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8"></div>
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta, que tal cadastrart uma agora
+        mesmo?
+      </p>
+      <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button>
+    </div>
   );
 }

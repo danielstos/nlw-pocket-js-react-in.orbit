ea8131ac4295335fd33f73602ce338a8a2b3ba18
danielstos <daniel.jose.stos@gmail.com>
2024-09-16 13:59:04 -0300

    feat: create getSummary and refactor App
    Created getSummary function to make a
    GET request and return the summary as
    a JSON object. The App component has
    been simplified: Uses getSummary to get
    and store the summary.Displays Summary if
    there is data, or EmptyGoals if there is
    not.The CreateGoal component is always
    displayed inside a dialog.

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,20 @@
 import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
 import { Summary } from "./components/summary";
-//import { EmptyGoals } from "./components/empty-goals";
+import { EmptyGoals } from "./components/empty-goals";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "./http/get-summary";
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ["summary"],
+    queryFn: getSummary,
+  });
+
   return (
     <Dialog>
-      {/*   <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data?.total > 0 ? <Summary /> : <EmptyGoals />}
+
       <CreateGoal />
     </Dialog>
   );

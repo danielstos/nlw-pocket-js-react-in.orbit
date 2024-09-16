56d4e4c363cf74eb4a3605fc98ddfddc5decc95f
danielstos <daniel.jose.stos@gmail.com>
2024-09-16 00:11:51 -0300

    refactor: styling EmptyGoals component

--- src/app.tsx
+++ src/app.tsx
@@ -1,28 +1,11 @@
-import logo from "./assets/logo-in-orbit.svg";
-import letsStart from "./assets/lets-start-illustration.svg";
-import { Plus } from "lucide-react";
-import { Button } from "./components/ui/button";
-import { Dialog, DialogTrigger } from "./components/ui/dialog";
+import { Dialog } from "./components/ui/dialog";
 import { CreateGoal } from "./components/create-goal";
+import { EmptyGoals } from "./components/empty-goals";
 
 export function App() {
   return (
     <Dialog>
-      <div className="h-screen flex flex-col items-center justify-center gap-8">
-        <img src={logo} alt="in.orbit" />
-        <img src={letsStart} alt="in.orbit" />
-        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-          Você ainda não cadastrou nenhuma meta, que tal cadastrart uma agora
-          mesmo?
-        </p>
-
-        <DialogTrigger asChild>
-          <Button>
-            <Plus className="size-4" />
-            Cadastrar meta
-          </Button>
-        </DialogTrigger>
-      </div>
+      <EmptyGoals />
       <CreateGoal />
     </Dialog>
   );
